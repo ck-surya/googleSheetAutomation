414715d8a8ba52784f51a17365ade26870f9b21c
anup <anup.kalbalia@gmail.com>
2024-09-25 17:22:56 +0500

    reafctoring WIP - phase 4

--- Código.js
+++ Código.js
@@ -34,31 +34,3 @@ function handleEdit(e) {
     markAsWithdrawn();
   }
 }
-
-function isValueEmpty(value) {
-  return value.length === 0;
-}
-
-function isTeacherTab(sheet, teacherEmailAndId) {
-  const teacherTabNames = teacherEmailAndId.map(tab => tab[1]);
-  return teacherTabNames.includes(sheet.getName());
-}
-
-function isStudentTab(sheet) {
-  return sheet.getName() === constants.STUDENT_TAB_NAME;
-}
-
-function getCellValue(tabName, cell) {
-  const tab = getTab(tabName);
-  const cellReference = cell;
-  const cellValue = tab.getRange(cellReference).getValue();
-  return cellValue;
-}
-
-function getMapForCourseSlot() {
-  return fetchCellValues(constants.META_TAB_NAME, constants.HOURS_CELL_REFRENCE_IN_META_SHEET, true);
-}
-
-function getSlotColumn() {
-  return fetchCellValues(constants.META_TAB_NAME, constants.SLOT_RANGE_IN_META_TAB, true);
-}
